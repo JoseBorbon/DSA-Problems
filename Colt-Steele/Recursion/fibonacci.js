aa99afa21bd9cb96f21e4ef313d0ec9020b5bc8b
JoseBorbon <joserborbon@gmail.com>
2021-09-28 09:32:40 -0400

    added recursive solution to fib with memoization

---
+++ Colt-Steele/Recursion/fibonacci.js
@@ -0,0 +1,19 @@
+/* Write a recursive function called fib which accepts a number and returns the th number in the Fibonacci sequence. Recall that the Fibonacci sequence 
+is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous 
+two numbers. */
+
+function fib(num, memo = {}) {
+  if (memo[num]) return memo[num];
+
+  if (num < 2) {
+    memo[num] = num;
+  } else {
+    memo[num] = fib(num - 1, memo) + fib(num - 2, memo);
+  }
+  return memo[num];
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
